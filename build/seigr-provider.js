63b2295ce0135492c6055de32f9939df5764b6ff
sergism <sergism@gmail.com>
2023-04-22 10:15:47 +0200

    build folder

--- build/seigr-provider.js
+++ build/seigr-provider.js
@@ -3,17 +3,16 @@ const express = require("express");
 const app = express();
 
 app.listen(53194, () => {
-
-    console.log("Listening on port 53194");
-
-});
-
+    
+        console.log("Listening on port 53194");
+    
+    }
+);
 
 const seigrProvider = new JsonRpcProvider("https://rpc.seigr.network");
 
 app.get("/seigr", async (req, res) => {
     const blockNumber = await seigrProvider.getBlockNumber();
     res.json({ blockNumber });
-});
-
-
+}
+);
