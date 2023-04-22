c355c6f6259698ee19f97700a2a55a3bd6547d8d
sergism <sergism@gmail.com>
2023-04-22 09:52:44 +0200

    html

--- seigr-provider.js
+++ seigr-provider.js
@@ -15,3 +15,7 @@ app.get("/seigr", async (req, res) => {
     const blockNumber = await seigrProvider.getBlockNumber();
     res.json({ blockNumber });
 });
+
+//should we create an html page for the provider to be displayed on? answer: yes
+//should we create an index.html page for the provider to be displayed on? answer: yes
+//what do we need in the index.html page? answer: the provider
